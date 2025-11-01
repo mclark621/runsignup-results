d5998e958a955211dad0b8a87e052e668a56ec8b
Mike Clark <mike@junction311.com>
2025-11-01 17:37:05 -0400

    Change oauth client id

--- ApiConfig.php
+++ ApiConfig.php
@@ -7,9 +7,9 @@ define('PROTOCOL', 'https');
 //define('TIMER_API_KEY', 'CDQoRxsMX2tqqi8eEnonTG4DCU5TKXZz');
 //define('TIMER_API_SECRET', 'kVXAe4IJVqpRkWwrw99XUzDeBHgFwsFV');
 
-define('RSU_CLIENT_ID', '55');
+define('RSU_CLIENT_ID', '84');
 define('RSU_CLIENT_SECRET', 'FZjJPD0+xjOHGKBH6KpXx4WarujMa9Fd0Y01G+RTC9g=');
-define('RSU_REDIRECT_URI', 'https://selfie.j311.co/callback.php'); 
+define('RSU_REDIRECT_URI', 'https://results.j311.co/callback.php'); 
 define('RSU_SCOPE', 'rsu_api_read'); 
 define('RSU_AUTH_ENDPOINT', 'https://runsignup.com/Profile/OAuth2/RequestGrant');
 define('RSU_TOKEN_ENDPOINT', 'https://runsignup.com/rest/v2/auth/auth-code-redemption.json');
