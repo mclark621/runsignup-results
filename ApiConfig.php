d9386c3bea336c362b24b52d61923e5911af521e
Mike Clark <mike@junction311.com>
2025-11-01 18:05:13 -0400

    Update Results Client ID & Secret for oauth

--- ApiConfig.php
+++ ApiConfig.php
@@ -7,8 +7,8 @@ define('PROTOCOL', 'https');
 //define('TIMER_API_KEY', 'CDQoRxsMX2tqqi8eEnonTG4DCU5TKXZz');
 //define('TIMER_API_SECRET', 'kVXAe4IJVqpRkWwrw99XUzDeBHgFwsFV');
 
-define('RSU_CLIENT_ID', '84');
-define('RSU_CLIENT_SECRET', 'FZjJPD0+xjOHGKBH6KpXx4WarujMa9Fd0Y01G+RTC9g=');
+define('RSU_CLIENT_ID', '86');
+define('RSU_CLIENT_SECRET', '55N+51msy6dLP0tVt1Flm2eI+XXa6PMPS2RDmc3L9aM=');
 define('RSU_REDIRECT_URI', 'https://results.j311.co/callback.php'); 
 define('RSU_SCOPE', 'rsu_api_read'); 
 define('RSU_AUTH_ENDPOINT', 'https://runsignup.com/Profile/OAuth2/RequestGrant');
